aa661fa51b173dbc95b115cdcf2d8a7e4d4d4711
bengyphan <bengyphan@gmail.com>
2021-01-07 11:40:26 -0800

    Updates

---
+++ precache-manifest.d198de431b9f50ae73c3f4974856a01b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4b39f2cf14cda109ede0d8bfa7c0b8",
+    "url": "/lights-out-game/index.html"
+  },
+  {
+    "revision": "5154682bb27fa2e01847",
+    "url": "/lights-out-game/static/css/main.b20083a0.chunk.css"
+  },
+  {
+    "revision": "c966725a35f2b2361ca2",
+    "url": "/lights-out-game/static/js/2.18b64473.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/lights-out-game/static/js/2.18b64473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5154682bb27fa2e01847",
+    "url": "/lights-out-game/static/js/main.d83417b1.chunk.js"
+  },
+  {
+    "revision": "7d96c8183e76d775c341",
+    "url": "/lights-out-game/static/js/runtime-main.eb92c67c.js"
+  }
+]);
